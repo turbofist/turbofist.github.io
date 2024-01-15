08bc7fb1456598b9a7aed32500581db5b1b0cde5
turbofist <137625553+turbofist@users.noreply.github.com>
2024-01-15 23:28:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 var input = document.getElementById("input");
 var output = document.getElementById("output");
+var mapObj = {
+  "COLOR(RED)":"<a style='color:red'>",
+  "COLOR(DEFAULT)":"<a style='color:white'>",
+  "COLOR(ADJUSTABLE)":"<a style='color:green'>",
+  "COLOR(BLUE)":"<a style='color:blue'>",
+  "COLOR(LIGHTBLUE)":"<a style='color:lightblue'>",
+  "COLOR(PURPLE)":"<a style='color:purple'>",
+  "COLOR(YELLOW)":"<a style='color:yellow'>",
+  "COLOR(BLACK)":"<a style='color:black'>",
+  TWO_CHOICE:"➜&nbsp;&nbsp;&nbsp;",
+  QUICKTEXT_ENABLE:"",
+  QUICKTEXT_DISABLE:"",
+  "\\n":"<br>",
+  "\"":""
+};
 input.addEventListener("keyup", updateText);
 function updateText(){
   document.getElementById("output").innerHTML = input.value;
+  input.value = input.value.replace(/\n/g, '');
+  document.getElementById("output").innerHTML = input.value.replace(/COLOR\(RED\)|COLOR\(DEFAULT\)|COLOR\(DEFAULT\)|COLOR\(ADJUSTABLE\)|COLOR\(BLUE\)|COLOR\(LIGHTBLUE\)|COLOR\(PURPLE\)|COLOR\(YELLOW\)|COLOR\(BLACK\)|TWO_CHOICE|QUICKTEXT_ENABLE|QUICKTEXT_DISABLE|\\n|\"/gi, function(matched){return mapObj[matched]});
 }
