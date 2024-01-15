968524f52a112533483d7a27799fc878138d55a9
Turbofist <turbofisted@proton.me>
2024-01-16 00:29:38 +0100

    a

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var input = document.getElementById("input");
+input.addEventListener("keyup", updateText);
 var output = document.getElementById("output");
 var mapObj = {
   "COLOR(RED)":"<a style='color:red'>",
@@ -9,16 +10,14 @@ var mapObj = {
   "COLOR(PURPLE)":"<a style='color:purple'>",
   "COLOR(YELLOW)":"<a style='color:yellow'>",
   "COLOR(BLACK)":"<a style='color:black'>",
-  TWO_CHOICE:"<a id=\"arrow\">➜&nbsp;&nbsp;&nbsp;</a>",
+  TWO_CHOICE:"➜&nbsp;&nbsp;&nbsp;",
   QUICKTEXT_ENABLE:"",
   QUICKTEXT_DISABLE:"",
-  BOX_BREAK:"<br><div class=\"boxbreak\">",
   "\\n":"<br>",
   "\"":""
 };
-input.addEventListener("keyup", updateText);
 function updateText(){
   document.getElementById("output").innerHTML = input.value;
   input.value = input.value.replace(/\n/g, '');
-  document.getElementById("output").innerHTML = input.value.replace(/COLOR\(RED\)|COLOR\(DEFAULT\)|COLOR\(DEFAULT\)|COLOR\(ADJUSTABLE\)|COLOR\(BLUE\)|COLOR\(LIGHTBLUE\)|COLOR\(PURPLE\)|COLOR\(YELLOW\)|COLOR\(BLACK\)|TWO_CHOICE|BOX_BREAK|QUICKTEXT_ENABLE|QUICKTEXT_DISABLE|\\n|\"/gi, function(matched){return mapObj[matched]});
+  document.getElementById("output").innerHTML = input.value.replace(/COLOR\(RED\)|COLOR\(DEFAULT\)|COLOR\(DEFAULT\)|COLOR\(ADJUSTABLE\)|COLOR\(BLUE\)|COLOR\(LIGHTBLUE\)|COLOR\(PURPLE\)|COLOR\(YELLOW\)|COLOR\(BLACK\)|TWO_CHOICE|QUICKTEXT_ENABLE|QUICKTEXT_DISABLE|\\n|\"/gi, function(matched){return mapObj[matched]});
 }
