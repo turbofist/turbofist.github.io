173ac8f1150f900e07872d8396a7234ed77f6c01
Turbofist <turbofisted@proton.me>
2024-01-16 15:09:33 +0100

    Fixed spaces

--- script.js
+++ script.js
@@ -4,17 +4,7 @@ if(input){
 }
 var output = document.getElementById("output");
 var mapObj = {
-  "COLOR\(RED\)":"<a style='color:red'>",
-  "COLOR\(DEFAULT\)":"<a style='color:white'>",
-  "COLOR\(ADJUSTABLE\)":"<a style='color:green'>",
-  "COLOR\(BLUE\)":"<a style='color:blue'>",
-  "COLOR\(LIGHTBLUE\)":"<a style='color:lightblue'>",
-  "COLOR\(PURPLE\)":"<a style='color:purple'>",
-  "COLOR\(YELLOW\)":"<a style='color:yellow'>",
-  "COLOR\(BLACK\)":"<a style='color:black'>",
   "TWO_CHOICE":"➜&nbsp;&nbsp;&nbsp;",
-  "QUICKTEXT_ENABLE":"",
-  "QUICKTEXT_DISABLE":"",
   "UNSKIPPABLE":"",
   "PERSISTENT":"",
   "\\n":"<br>",
@@ -31,8 +21,8 @@ var iconMap = {
   28:"<img class=\"ico\" src=\"img/28.png\">"
 }
   function replaceShift(matched,p1,offset,string){
-    var n = parseInt(p1);
-    var n = Math.floor(p1/3);
+    var n = parseInt(p1, 16);
+    var n = Math.floor(p1/4);
     var string = "";
     for(var i = 0; i<n; i++){
       string += " ";
@@ -51,7 +41,27 @@ function updateText(){
   if(input.value)
     document.getElementById("output").innerHTML = input.value;
   input.value = input.value.replace(/\n/g, '');
-  document.getElementById("output").innerHTML = input.value.replace(/(COLOR\(RED\)|COLOR\(DEFAULT\)|COLOR\(DEFAULT\)|COLOR\(ADJUSTABLE\)|COLOR\(BLUE\)|COLOR\(LIGHTBLUE\)|COLOR\(PURPLE\)|COLOR\(YELLOW\)|COLOR\(BLACK\)|TWO_CHOICE|UNSKIPPABLE|QUICKTEXT_ENABLE|QUICKTEXT_DISABLE|PERSISTENT|\\n|\")/gi, function(matched){return mapObj[matched]}).replace(/DEFINE_MESSAGE\(.*? /gi, '').replace(/TEXTBOX_TYPE_BLACK, /gi, '<div id="TEXTBOX_TYPE_BLACK"></div>').replace(/TEXTBOX_TYPE_BLUE,/gi, '<div id="TEXTBOX_TYPE_BLUE"></div>').replace(/TEXTBOX_POS_TOP,/gi, '<div id="TEXTBOX_POS_TOP"></div>').replace(/TEXTBOX_POS_MIDDLE,/gi, '<div id="TEXTBOX_POS_MIDDLE"></div>').replace(/TEXTBOX_POS_BOTTOM,/gi, '<div id="TEXTBOX_POS_BOTTOM"></div>').replace(/TEXTBOX_POS_VARIABLE,/gi, '<div id="TEXTBOX_POS_VARIABLE"></div>').replace(/SHIFT\(\\x(..)\)/gi, replaceShift).replace(/BOX_BREAK/gi, boxBreak).replace(/ITEM_ICON\(\\x(..)\)/gi, codeToIcon);
+  document.getElementById("output").innerHTML = input.value.replace(/TWO_CHOICE|UNSKIPPABLE|PERSISTENT|\\n|\"/gi, function(matched){return mapObj[matched]})
+    .replace(/DEFINE_MESSAGE\(.*? /gi, '')
+    .replace(/\s*QUICKTEXT_ENABLE\s*/gi,'')
+    .replace(/\s*QUICKTEXT_DISABLE\s*/gi,'')
+    .replace(/\sCOLOR\(RED\)\s/gi, '<a style=\'color:red\'>')
+    .replace(/\sCOLOR\(DEFAULT\)\s/gi, '<a style=\'color:white\'>')
+    .replace(/\sCOLOR\(ADJUSTABLE\)\s/gi, '<a style=\'color:green\'>')
+    .replace(/\sCOLOR\(BLUE\)\s/gi, '<a style=\'color:blue\'>')
+    .replace(/\sCOLOR\(LIGHTBLUE\)\s/gi, '<a style=\'color:lightblue\'>')
+    .replace(/\sCOLOR\(PURPLE\)\s/gi, '<a style=\'color:purple\'>')
+    .replace(/\sCOLOR\(YELLOW\)\s/gi, '<a style=\'color:yellow\'>')
+    .replace(/\sCOLOR\(BLACK\)\s/gi, '<a style=\'color:black\'>')
+    .replace(/\s*TEXTBOX_TYPE_BLACK,\s*/gi, '<div id="TEXTBOX_TYPE_BLACK"></div>')
+    .replace(/\s*TEXTBOX_TYPE_BLUE,\s*/gi, '<div id="TEXTBOX_TYPE_BLUE"></div>')
+    .replace(/\s*TEXTBOX_POS_TOP,\s*/gi, '<div id="TEXTBOX_POS_TOP"></div>')
+    .replace(/\s*TEXTBOX_POS_MIDDLE,\s*/gi, '<div id="TEXTBOX_POS_MIDDLE"></div>')
+    .replace(/\s*TEXTBOX_POS_BOTTOM,\s*/gi, '<div id="TEXTBOX_POS_BOTTOM"></div>')
+    .replace(/\s*TEXTBOX_POS_VARIABLE\s*,/gi, '<div id="TEXTBOX_POS_VARIABLE"></div>')
+    .replace(/SHIFT\(\\x(..)\)/gi, replaceShift)
+    .replace(/BOX_BREAK/gi, boxBreak)
+    .replace(/ITEM_ICON\(\\x(..)\)/gi, codeToIcon);
   if($("#TEXTBOX_TYPE_BLACK").length){
     $("#output").css("background-color", "#555");
   }
